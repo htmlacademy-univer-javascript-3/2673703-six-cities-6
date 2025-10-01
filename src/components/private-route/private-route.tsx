48d5f68f3fa66e071770bfe0ee76f77e59ce3170
MeTStIL <stme560@gmail.com>
2025-10-01 17:47:37 +0500

    dz3

---
+++ src/components/private-route/private-route.tsx
@@ -0,0 +1,19 @@
+﻿import {AppRoute, AuthorizationStatus} from '../../cosnt.ts';
+import {Navigate} from 'react-router-dom';
+
+
+type PrivateRouteProps = {
+  authorizationStatus: AuthorizationStatus;
+  children: JSX.Element;
+}
+
+
+function PrivateRoute({authorizationStatus, children} : PrivateRouteProps) {
+  return (
+    authorizationStatus === AuthorizationStatus.Auth
+      ? children
+      : <Navigate to={AppRoute.Login} />
+  );
+}
+
+export default PrivateRoute;

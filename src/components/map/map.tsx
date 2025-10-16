6f264a2bd4b6bc44038bfa6e76b944a46b33ba36
MeTStIL <stme560@gmail.com>
2025-10-16 23:50:33 +0500

    fix chosenId + map component

---
+++ src/components/map/map.tsx
@@ -0,0 +1,15 @@
+﻿
+
+type MapProps = {
+  chosenId: string | null;
+};
+
+function Map({chosenId}: MapProps) {
+  return (
+    <div className="cities__right-section">
+      <section className={`cities__map map ${chosenId}`}></section>
+    </div>
+  );
+}
+
+export default Map;
